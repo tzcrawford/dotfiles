be3e530868ad27a0a8baae2c9c45b759ca6d8e55
tzcrawford <t.z.crawford@hotmail.com>
2020-05-17 21:34:35 -0700

    should probably try before uploading

--- .vim/get.sh
+++ .vim/get.sh
@@ -3,14 +3,17 @@
 VIMDIR=~/.vim/
 
 #using pathogen to install addons!
-mkdir $VIMDIR/autoload > /dev/null 2>&1
+mkdir $VIMDIR/autoload > /dev/null 2>&1 #make the directory if possible and ignore output
 cd $VIMDIR/autoload
 git clone https://github.com/tpope/vim-pathogen.git
+mv vim-pathogen/autoload/* .
+sudo rm -r vim-pathogen
 #add to vimrc (uncommented):
 #execute pathogen#infect()
 #execute pathogen#helptags()
 
 #install vim autocomlpop
+mkdir $VIMDIR/bundle > /dev/null 2>&1
 cd $VIMDIR/bundle
 git clone https://github.com/vim-scripts/AutoComplPop
 #make sure to add/enable options in vimrc
