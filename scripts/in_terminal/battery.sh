72ddb528f353db028f714bf0bb5fcfe9a55a45ed
tzcrawford <t.z.crawford@hotmail.com>
2020-09-24 12:59:53 -0700

    too lazy to leave comment, just general recent changes

--- scripts/in_terminal/battery.sh
+++ scripts/in_terminal/battery.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#returns your batter charge percent
+#returns your battery charge percent
 if lsb_release -a | grep "Arch Linux" > /dev/null; then
     if ! pacman -Qi acpi > /dev/null; then
         sudo pacman -S acpi
