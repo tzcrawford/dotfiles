96288498ad80a8d62de0ea7b4762fb3fcd2bf955
tzcrawford <z@tzcrawford.com>
2023-05-13 14:42:51 -0700

    gpustats.sh: Altered kernel calls for VRAM memory to match what my new RX 6750 GPU is apparently printing with kernel 6.2.12

--- scripts/in_terminal/gpustats.sh
+++ scripts/in_terminal/gpustats.sh
@@ -1,11 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 #Prints information about GPU usage
 
 #VLOAD is the percent of cores being utilized (I think)
 #VRAMRAT is the percent of VRAM being utilized
 #VLOAD=$(sudo cat /sys/kernel/debug/dri/0/amdgpu_pm_info | grep "GPU Load:" | sed -e 's/GPU Load: //' | sed -e 's/ //')
-USEDVRAM=$(sudo cat /sys/kernel/debug/dri/0/amdgpu_vram_mm | grep "total:" | sed -e 's/,//' | cut -d ' ' -f 4)
-TOTALVRAM=$(sudo cat /sys/kernel/debug/dri/0/amdgpu_vram_mm | grep "total:" | sed -e 's/,//' | cut -d ' ' -f 2)
+#USEDVRAM=$(sudo cat /sys/kernel/debug/dri/0/amdgpu_vram_mm | grep "total:" | sed -e 's/,//' | cut -d ' ' -f 4)
+#TOTALVRAM=$(sudo cat /sys/kernel/debug/dri/0/amdgpu_vram_mm | grep "total:" | sed -e 's/,//' | cut -d ' ' -f 2)
+
+USEDVRAM="$(sudo cat /sys/kernel/debug/dri/0/amdgpu_vram_mm | grep -E "^ *usage:" | cut -d ' ' -f 4)"
+TOTALVRAM="$(sudo cat /sys/kernel/debug/dri/0/amdgpu_vram_mm | grep -E "^ *size:"  | cut -d ' ' -f 4)"
+
 #echo $VLOAD
 #echo $USEDVRAM
 #echo $TOTALVRAM
