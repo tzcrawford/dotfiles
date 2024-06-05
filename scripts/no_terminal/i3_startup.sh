d23ae35d1ccb4f7e5b246bdb1c244f8b16bdaeb3
tzcrawford <z@tzcrawford.com>
2024-06-05 13:28:57 -0700

    Updated i3 startup config and workspace-to-monitor binding.

--- scripts/no_terminal/i3_startup.sh
+++ scripts/no_terminal/i3_startup.sh
@@ -1,18 +1,25 @@
 #!/bin/sh
-i3-msg 'workspace 11; exec pavucontrol'
-sleep 2
-i3-msg 'workspace 11; exec alacritty -e bluetoothctl'
-sleep 2
-i3-msg 'workspace 12; exec alacritty -e ~/scripts/in_terminal/email.sh'
-sleep 2
-#i3-msg 'workspace 0; exec ~/scripts/no_terminal/musicplayer.sh' #urxvt doesn't theme correctly
+if neofetch | grep -i "WM.*i3" ; then
+    wm_msgr=i3-msg
+elif neofetch | grep -i "WM.*sway" ; then
+    wm_msgr=swaymsg
+else
+    exit
+fi
 
-i3-msg 'workspace 1; exec qutebrowser'
+$wm_msgr 'workspace 11; exec pavucontrol'
+sleep 2
+$wm_msgr 'workspace 11; exec alacritty -e bluetoothctl'
+sleep 2
+$wm_msgr 'workspace 12; exec alacritty -e ~/scripts/in_terminal/email.sh'
+sleep 2
+#$wm_msgr 'workspace 0; exec ~/scripts/no_terminal/musicplayer.sh' #urxvt doesn't theme correctly
+
+$wm_msgr 'workspace 1; exec qutebrowser'
 sleep 3
-i3-msg 'workspace 9; exec discord'
-sleep 10
-i3-msg 'workspace 1'
+$wm_msgr 'workspace 9; exec discord'
+sleep 5
+$wm_msgr 'workspace 1'
 
-#i3-msg 'exec ~/.config/polybar/polybarstart.sh'
-i3-msg 'restart' # IDK, polybar just isn't starting on intial startup...
+$wm_msgr 'restart' # IDK, polybar just isn't starting on intial startup...
 
