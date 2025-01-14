30cc6810e9581251c3457fbf7736767c5831ab76
tzcrawford <z@tzcrawford.com>
2025-01-13 18:28:53 -0700

    Created archlinux_packages_usually_installed.sh to help with install on new system.

---
+++ archlinux_packages_usually_installed.sh
@@ -0,0 +1,9 @@
+#sudo pacman -S 7zip arch-install-scripts archlinux-keyring bash-completion bluez bluez-utils bzip2 cmake cups cups-pdf curl dash dmenu ffmpeg git gnupg gzip htop imagemagick lvm2 mesa nano networkmanager nodejs npm ntfs-3g pacman-mirrorlist pinentry pv subversion sudo tmux ufw unrar unzip vim wget x264 x265 || exit
+#sudo pacman -S arandr alacritty android-file-transfer android-tools feh carla flac flatpak fontconfig fzf gcc gedit gst-plugin-gtk gst-plugin-pipewire gst-plugins-bad gst-plugins-bad-libs gst-plugins-base gst-plugins-base-libs gst-plugins-good gst-plugins-ugly gst-python gstreamer gvim i3-wm i3blocks i3lock i3status mpd ncurses neofetch neomutt nm-connection-editor noto-fonts noto-fonts-cjk noto-fonts-emoji otf-font-awesome otf-libertinus nsxiv pdf2svg picom pipewire pipewire-jack pipewire-pulse polybar rxvt-unicode thunar ttf-dejavu ttf-hack ttf-inconsolata ttf-joypixels ttf-liberation ttf-linux-libertine ttf-roboto xclip xdg-desktop-portal xdg-desktop-portal-gtk xdg-desktop-portal-xapp xdg-user-dirs xdg-utils xorg zathura zathura-djvu zathura-pdf-mupdf || exit
+#sudo pacman -S calcurse calibre chromium firefox gnu-free-fonts gnuplot inkscape minidlna mypaint ncmpcpp neovim newsboat obs-studio okular qjackctl steam sway texlive thunderbird weechat wine-mono wine-staging winetricks xournalpp || exit
+#sudo systemctl enable --now NetworkManager || exit
+#sudo systemctl enable --now bluetooth || exit
+#sudo ln -s /bin/dash /bin/sh || exit
+#mkdir ~/build && cd ~/build && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
+#yay -S abook downgrade flatseal gimp-devel localsend-bin miniconda3 pdf2png puddletag rxvt-unicode-pixbuf sc-im-git spotify spotifyd steamlink surf ttf-envy-code-r ttf-font-awesome ttf-ms-fonts ttf-symbola urxvt-font-size-git urxvt-perls v4l-utils v4l2loopback-dkms xf86-input-wacom yt-dlp zoom
+
