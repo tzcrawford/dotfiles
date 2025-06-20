ae4bc5450100187f287b8f46ee89b536acb1065c
tzcrawford <z@tzcrawford.com>
2025-06-19 22:41:47 -0700

    scripts/themes/*: Added wayland/hyprland support for themeing

--- scripts/themes/i3_theme_slideshow.sh
+++ scripts/themes/i3_theme_slideshow.sh
@@ -2,11 +2,18 @@
 #randomly picks an image in ~/Pictures/wallpaper_active/ and sets it as the i3 wallpaper and automatically generates colorscheme palette from image
 
 randomfunction(){
-    ls ~/Pictures/wallpaper_active | sort -R | tail -1 | while read file
+    ls $HOME/Pictures/wallpaper_active | sort -R | tail -1 | while read file
     do echo $file
     done
 }
 file2=$(randomfunction)
-#DISPLAY=:0.0 feh -B white --bg-fill ~/Pictures/wallpaper_active/$file2
-wal -i ~/Pictures/wallpaper_active/$file2
-#crontab ~/scripts/dotfiles/crontab_user
+echo $file2
+file3="$HOME/Pictures/wallpaper_active/$file2"
+echo $file3
+#DISPLAY=:0.0 feh -B white --bg-fill $HOME/Pictures/wallpaper_active/$file2
+if env | grep "WAYLAND_DISPLAY" > /dev/null ; then
+    hyprctl hyprpaper reload ,"$file3"
+fi
+wal -i "$file3"
+#crontab $HOME/scripts/dotfiles/crontab_user
+
--- scripts/themes/themepick.sh
+++ scripts/themes/themepick.sh
@@ -1,8 +1,5 @@
-#!/bin/sh
-#an i3 script I wrote to set the background and the colorscheme
-
-#temp=$(echo -e "Rotating Wallpaper\nGrayscale\nSelect" | dmenu -i -l 15 -fn Monospace-18 -nb '#2c2c2e' -nf '#ff944d' -sb '#040404' -sf '#ff6600')
-temp=$(echo -e "Rotating Wallpaper\nGrayscale\nSelect" | dmenu -i -l 15 -fn Monospace-18 -nb '#2c2c2e' -nf '#c6cc5e' -sb '#040404' -sf '#eaf51e')
+#!/bin/bash
+temp=$(echo -e "Rotating Wallpaper\nGrayscale\nSelect" | ~/scripts/dmenu.sh -y )
 if [ "$temp" = "Rotating Wallpaper" ]; then
         ~/scripts/themes/i3_theme_slideshow.sh
 elif [ "$temp" = "Grayscale" ]; then
@@ -13,3 +10,4 @@ else
         :
 fi
 unset temp
+
