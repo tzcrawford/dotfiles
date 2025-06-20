01a9253cfb4fdfc9e02e3dd07f7a23c3bf4524c9
tzcrawford <z@tzcrawford.com>
2025-06-19 18:40:03 -0700

    scripts/dmenu.sh: Updated to support bemenu on wayland

--- scripts/dmenu.sh
+++ scripts/dmenu.sh
@@ -14,36 +14,73 @@ fi
 # Set default values
 COLORS=('#2c2c2e' '#ffffff' '#040404' '#bbbbbb') # White
 PROMPT=""
+RUNMODE=false
+
+for arg in "$@"; do
+    if [[ "$arg" == "--run" ]]; then
+        RUNMODE=true
+        # Remove this argument from the list
+        set -- "${@[@]/$arg}"
+    fi
+done
 
 while getopts 'ogbvryp:' OPTION ; do # Process option provided flags
     case "$OPTION" in
         o) # Sets color
-            COLORS=('#2c2c2e' '#ff944d' '#040404' '#ff6600') # Orange
+            COLORS=('#2c2c2e' '#ff944d' '#040404' '#ff6600' '#272729') # Orange
             ;;
         g) # Sets color
-            COLORS=('#2c2c2e' '#73ba7f' '#040404' '#24e344') # Green
+            COLORS=('#2c2c2e' '#73ba7f' '#040404' '#24e344' '#272729') # Green
             ;;
         b) # Sets color
-            COLORS=('#2c2c2e' '#00aacc' '#040404' '#00d0ff') # Blue
+            COLORS=('#2c2c2e' '#00aacc' '#040404' '#00d0ff' '#272729') # Blue
             ;;
         v) # Sets color
-            COLORS=('#2c2c2e' '#c279c4' '#040404' '#f700ff') # Purple
+            COLORS=('#2c2c2e' '#c279c4' '#040404' '#f700ff' '#272729') # Purple
             ;;
         r) # Sets color
-            COLORS=('#2c2c2e' '#ff2222' '#040404' '#ff0000') # Purple
+            COLORS=('#2c2c2e' '#ff2222' '#040404' '#ff0000' '#272729') # Red
             ;;
         y) # Sets color
-            COLORS=('#2c2c2e' '#ff944d' '#040404' '#ff6600') # Yellow
+            COLORS=('#2c2c2e' '#ff944d' '#040404' '#ff6600' '#272729') # Yellow
             ;;
         p) # Gives prompt
             PROMPT=$OPTARG
             ;;
         ?)
-            COLORS=('#2c2c2e' '#ffffff' '#040404' '#bbbbbb') # White
+            COLORS=('#2c2c2e' '#ffffff' '#040404' '#bbbbbb' '#272729') # White
             ;;
     esac
 done
 shift "$(($OPTIND -1))" # Reset the options flag index
 
-dmenu -i -p "$PROMPT" -l $NUMLINES -fn $FONTFAMILY-$FONTSIZE -nb ${COLORS[0]} -nf ${COLORS[1]} -sb ${COLORS[2]} -sf ${COLORS[3]}
+if env | grep "WAYLAND_DISPLAY" > /dev/null ; then
+    if $RUNMODE ; then
+        bemenu-run -i -p "$PROMPT" -l "$NUMLINES" \
+            --fn "$FONTFAMILY ${FONTSIZE}" \
+            --tb "${COLORS[0]}" --tf "${COLORS[1]}" \
+            --nb "${COLORS[0]}" --nf "${COLORS[1]}" \
+            --ab "${COLORS[4]}" --af "${COLORS[1]}" \
+            --fb "${COLORS[0]}" --ff "${COLORS[1]}" \
+            --sb "${COLORS[2]}" --sf "${COLORS[3]}" \
+            --cb "${COLORS[1]}" --cf "${COLORS[0]}" \
+            --hb "${COLORS[2]}" --hf "${COLORS[3]}"
+    else
+        bemenu -i -p "$PROMPT" -l "$NUMLINES" \
+            --fn "$FONTFAMILY ${FONTSIZE}" \
+            --tb "${COLORS[0]}" --tf "${COLORS[1]}" \
+            --nb "${COLORS[0]}" --nf "${COLORS[1]}" \
+            --ab "${COLORS[4]}" --af "${COLORS[1]}" \
+            --fb "${COLORS[0]}" --ff "${COLORS[1]}" \
+            --sb "${COLORS[2]}" --sf "${COLORS[3]}" \
+            --cb "${COLORS[1]}" --cf "${COLORS[0]}" \
+            --hb "${COLORS[2]}" --hf "${COLORS[3]}"
+    fi
+else
+    if $RUNMODE ; then
+        dmenu-run -i -p "$PROMPT" -l $NUMLINES -fn $FONTFAMILY-$FONTSIZE -nb ${COLORS[0]} -nf ${COLORS[1]} -sb ${COLORS[2]} -sf ${COLORS[3]}
+    else
+        dmenu -i -p "$PROMPT" -l $NUMLINES -fn $FONTFAMILY-$FONTSIZE -nb ${COLORS[0]} -nf ${COLORS[1]} -sb ${COLORS[2]} -sf ${COLORS[3]}
+    fi
+fi
 
