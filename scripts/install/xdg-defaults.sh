a3b3938f749a12c9585f828507ae63d0d57c4efa
T. Zack Crawford <z@tzcrawford.com>
2025-09-23 17:41:29 -0700

    resolved zathura not working for xdg-open on new installs, need to create a desktop file and put in /usr/share/applications/.

--- scripts/install/xdg-defaults.sh
+++ scripts/install/xdg-defaults.sh
@@ -10,11 +10,14 @@
 #returns "gimp.desktop" etc
 
 #change the default application for a MIME type
-xdg-mime default zathura.desktop application/pdf application/oxps application/epub_zip
+xdg-mime default zathura.desktop application/pdf application/oxps application/epub_zip applica/pdf 
 xdg-mime default sxiv.desktop image/png image/jpeg
 xdg-mime default mpv.desktop video/x-matroska video/mp4 video/x-msvideo video/webm video/x-flv video/quicktime video/x-ms-wmv video/MP2T application/x-mpegURL video/3gpp audio/aiff audio/midi audio/mpeg audio/mpeg3 audio/basic audio/wav
-xdg-mime default qutebrowser.desktop text/html message/rfc822
-xdg-settings set default-web-browser org.qutebrowser.qutebrowser.desktop #seems to be a fix issues with http links resorting to other browsers
+if [ $BROWSER = "" ] ; then
+    xdg-mime default qutebrowser.desktop text/html message/rfc822
+fi
+#xdg-settings set default-web-browser org.qutebrowser.qutebrowser.desktop #seems to be a fix issues with http links resorting to other browsers
 
 #be aware, $APPLICATION.desktop must exist in /usr/share/applications
 #generally included with installation, but can also be manually made
+
