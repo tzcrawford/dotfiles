948abea0744dcdd47b4386dcbe8d539244038ce3
T. Zack Crawford <z@tzcrawford.com>
2025-10-03 10:40:37 -0700

    scripts/install/xdg-defaults: Updated with new defaults for opening images in nsxiv and web links in qutebrowser.     - Switched sxiv.desktop reference to nsxiv since the original sxiv project has been abandoned and replaced by nsxiv     - Added configuration for x-scheme-handler/http* to open in qutebrowser. But qutebrowser does not have a desktop entry on my system, so I created one and dropped it in `/usr/share/applications`.

--- scripts/install/xdg-defaults.sh
+++ scripts/install/xdg-defaults.sh
@@ -11,13 +11,17 @@
 
 #change the default application for a MIME type
 xdg-mime default zathura.desktop application/pdf application/oxps application/epub_zip applica/pdf 
-xdg-mime default sxiv.desktop image/png image/jpeg
+xdg-mime default nsxiv.desktop image/png image/jpeg
 xdg-mime default mpv.desktop video/x-matroska video/mp4 video/x-msvideo video/webm video/x-flv video/quicktime video/x-ms-wmv video/MP2T application/x-mpegURL video/3gpp audio/aiff audio/midi audio/mpeg audio/mpeg3 audio/basic audio/wav
 xdg-mime default libreoffice-writer.desktop application/vnd.openxmlformats-officedocument.wordprocessingml.document application/msword application/vnd.oasis.opendocument.text
 if [ $BROWSER = "" ] ; then
     xdg-mime default qutebrowser.desktop text/html message/rfc822
 fi
 #xdg-settings set default-web-browser org.qutebrowser.qutebrowser.desktop #seems to be a fix issues with http links resorting to other browsers
+
+# These allow installed electron apps to open links in qutebrowser.
+xdg-mime default qutebrowser.desktop x-scheme-handler/http
+xdg-mime default qutebrowser.desktop x-scheme-handler/https
 
 #be aware, $APPLICATION.desktop must exist in /usr/share/applications
 #generally included with installation, but can also be manually made
