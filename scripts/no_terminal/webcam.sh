3740e73e18a50eb1700ec91e7e81c6283b0b19be
tzcrawford <z@tzcrawford.com>
2025-11-22 19:31:07 -0700

    scripts/no_terminal/webcam.sh added wayland support.

--- scripts/no_terminal/webcam.sh
+++ scripts/no_terminal/webcam.sh
@@ -1,4 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 #spawns mpv showing your webcam feed
 
-mpv av://v4l2:/dev/video0
+var="$(echo -e "0\n1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12" | ~/scripts/dmenu.sh)"
+
+mpv av://v4l2:/dev/video${var}
