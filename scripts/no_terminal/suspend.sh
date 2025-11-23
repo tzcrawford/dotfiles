eccfbf46a45714017d975113309cae3a2db05e4e
tzcrawford <z@tzcrawford.com>
2025-11-22 19:57:05 -0700

    Updated scripts/no_terminal/suspend.sh with hyprland support.

--- scripts/no_terminal/suspend.sh
+++ scripts/no_terminal/suspend.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 # Tells the computer to temporarily sleep
 
-i3lock && sleep 2 && systemctl suspend
+if [ "$XDG_CURRENT_DESKTOP" = "Hyprland" ] ; then
+    swaylock && sleep 2 && systemctl suspend
+else
+    i3lock && sleep 2 && systemctl suspend
+fi
