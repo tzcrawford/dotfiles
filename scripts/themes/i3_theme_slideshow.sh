613e974b1ae14ddca325e69c7a7b75d53e452855
T. Zack Crawford <z@tzcrawford.com>
2026-01-11 17:15:08 -0700

    Updated hyprpaper wallpaper setting script to reflect new expectations.

--- scripts/themes/i3_theme_slideshow.sh
+++ scripts/themes/i3_theme_slideshow.sh
@@ -12,7 +12,7 @@ file3="$HOME/Pictures/wallpaper_active/$file2"
 echo $file3
 #DISPLAY=:0.0 feh -B white --bg-fill $HOME/Pictures/wallpaper_active/$file2
 if env | grep "WAYLAND_DISPLAY" > /dev/null ; then
-    hyprctl hyprpaper reload ,"$file3"
+    hyprctl hyprpaper wallpaper ,"$file3"
 fi
 wal -i "$file3"
 #crontab $HOME/scripts/dotfiles/crontab_user
