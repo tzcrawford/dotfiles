f820db1829d26b75470279b30fa92c275448ab5b
tzcrawford <z@tzcrawford.com>
2026-08-08 14:22:21 -0700

    Added support for passmenu to return OTP passwords.

--- scripts/no_terminal/passmenu.sh
+++ scripts/no_terminal/passmenu.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # Aesthetic dmenu options for retrieving passwords from the password manager "pass"
 # Fork of https://git.zx2c4.com/password-store/tree/contrib/dmenu/passmenu
+# Supports:
+#   --type       Type the password instead of copying it
+#   --otp        Retrieve OTP instead of password
+#   --otp --type Type OTP instead of copying it
 
 # We could use the following to instead use that script.
 #passmenu -i -l 15 -fn EnvyCodeR-20 -nb '#2c2c2e' -nf '#00aacc' -sb '#040404' -sf '#00d0ff'
@@ -9,17 +13,29 @@
 shopt -s nullglob globstar
 
 typeit=0
-if [[ $1 == "--type" ]]; then
-	typeit=1
-	shift
-fi
+otp=0
+
+# Parse our options before passing remaining arguments to dmenu.
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        --type)
+            typeit=1
+            shift
+            ;;
+        --otp)
+            otp=1
+            shift
+            ;;
+        *)
+            break
+            ;;
+    esac
+done
 
 if [[ -n $WAYLAND_DISPLAY ]]; then
-	#dmenu=dmenu-wl # original code
         dmenu=~/scripts/dmenu.sh
 	xdotool="ydotool type --file -"
 elif [[ -n $DISPLAY ]]; then 
-        #dmenu=dmenu # original code
         dmenu=~/scripts/dmenu.sh
 	xdotool="xdotool type --clearmodifiers --file -"
 else
@@ -37,8 +53,19 @@ password=$(printf '%s\n' "${password_files[@]}" | "$dmenu" -b "$@") # We add our
 
 [[ -n $password ]] || exit
 
-if [[ $typeit -eq 0 ]]; then
-	pass show -c "$password" 2>/dev/null
+if [[ $otp -eq 1 ]]; then
+    # OTP mode
+    if [[ $typeit -eq 0 ]]; then
+        pass otp -c "$password" 2>/dev/null
+    else
+        pass otp "$password" | $xdotool
+    fi
 else
+    # Normal password mode
+    if [[ $typeit -eq 0 ]]; then
+        pass show -c "$password" 2>/dev/null
+    else
 	pass show "$password" | { IFS= read -r pass; printf %s "$pass"; } | $xdotool
+    fi
 fi
+
